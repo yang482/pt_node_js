22373180e2bb1504064ff927f8715b4c37b0172c
yang482 <yang482@gmail.com>
2017-08-14 16:42:01 +0900

    clear sorurce

--- routes/route.js
+++ routes/route.js
@@ -2,11 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const index = require('./index');
-const users = require('./users');
 const chat = require('./chatting/chat');
 
 router.use('/'      , index);
-router.use('/users' , users);
 router.use('/chat'  , chat);
 
 module.exports = router;
