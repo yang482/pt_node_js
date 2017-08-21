3e0e407473b6de1810450547608378bee5b9a55d
yang482 <yang482@gmail.com>
2017-08-21 15:55:36 +0900

    simple source change

--- app.js
+++ app.js
@@ -23,20 +23,21 @@ app.use('/', require('./routes/route'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.errStatus = err.status;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error/error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error/error');
 });
 
 module.exports = app;
--- routes/chatting/chat.js
+++ routes/chatting/chat.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('chat/client', { title:'Chatting' });
+    res.render('chat/client', { title:'Chatting' });
 });
 
 module.exports = router;
